9f203c8203dee00850ddf60d76c5cb4780c8472b
Francesco <francescocarvelli25@gmail.com>
2023-11-20 23:08:35 +0100

    finished the hero section in the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { HeroBlob } from "../assets";
+import Carousel from "../components/Carousel";
+import { HomeCarouselImages } from "../assets/Carousel_Images";
 
 const Home = () => {
   return (
-    <div className="flex gap-x-10">
-      <div className="flex-1 flex flex-col gap-y-6">
+    <div className="flex justify-between content-center gap-x-5 py-16">
+      <div className="flex-[0.5] flex flex-col justify-center gap-y-6">
         <h2 className="leading-[1.2] text-6xl">
           <p
             className="
@@ -30,15 +33,52 @@ const Home = () => {
             Savio
           </p>
         </h2>
-        <p className="text-neutral-600">
+        <p
+          className="
+        text-neutral-600 
+        leading-[1.5] 
+        tracking-wide 
+        text-justify
+        "
+        >
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
         </p>
       </div>
-      <div className="flex-1">
-        
+      <div className="flex-[0.4] relative">
+        <div
+          className="
+        w-[70%]
+        overflow-hidden
+        rounded-xl 
+        shadow-[8px_16px_25px_#1f1f1f99]
+        relative
+        z-20
+        "
+        >
+          <Carousel
+            elementsTotal={HomeCarouselImages.length}
+            visibleElements={1}
+            animate={true}
+            showButtons={false}
+          >
+            {HomeCarouselImages.map((image, idx) => (
+              <img className="w-full" key={idx} src={image} alt={"Image" + 1} />
+            ))}
+          </Carousel>
+        </div>
+        <img
+          className="
+          absolute
+          right-0
+          top-16
+          z-10     
+          "
+          src={HeroBlob}
+          alt="Hero-Blob"
+        />
       </div>
     </div>
   );
