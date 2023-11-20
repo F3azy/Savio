5963186babb2d17d3e0512a6b6cc011fe0765b45
Francesco <francescocarvelli25@gmail.com>
2023-11-20 19:46:12 +0100

    started to adjust the ui of the page, begin from the layout

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Layout = () => {
   return (
     <>
-    <Outlet />
+      <Navbar />
+      <main
+        className="
+        px-[16px] 2xl:px-[340px] 
+        py-[12px] 2xl:py-[18px]
+      "
+      >
+        <Outlet />
+      </main>
     </>
-  )
+  );
 };
 
 export default Layout;
