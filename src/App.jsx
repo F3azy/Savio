f5a4493753b7a4c57b2ab54a58513691b51a7539
Francesco <francescocarvelli25@gmail.com>
2023-11-20 16:23:31 +0100

    created the project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Gallery from "./pages/Gallery";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route element={<Gallery />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
