5bd88c54e3b1ce6d2af0fc2cc700acbf4640f98b
Francesco <francescocarvelli25@gmail.com>
2023-11-22 16:52:19 +0100

    made a sections folder for home page sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Hero from "../components/Hero";
-import About from "../components/About";
+import Hero from "../sections/Hero";
+import About from "../sections/About";
 
 const Home = () => {
   return (
