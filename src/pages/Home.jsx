efd91f661eea32727fd554834d1535ccb7513389
Francesco <francescocarvelli25@gmail.com>
2023-11-22 19:41:33 +0100

    added the team section to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../sections/Hero";
 import About from "../sections/About";
+import Team from "../sections/Team";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <About />
+      <Team />
     </>
   );
 };
---
+++ src/sections/Team.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import TeamMemberCard from "../components/TeamMemberCard";
+import Image1 from '../assets/Carousel_Images/Image1.png'
+
+const Team = () => {
+  return (
+    <section className="w-full flex flex-col items-center gap-y-6 py-14">
+      <h3
+        className="
+        font-normal 
+        text-[14px] 
+      "
+      >
+        | Nasz Skład
+      </h3>
+      <div className="w-full grid grid-cols-2 gap-10">
+        <TeamMemberCard image={Image1} name={"Jan Kowalski"} desription={`          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.`} />
+        <TeamMemberCard image={Image1} name={"Jan Kowalski"} desription={`          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.`} />
+        <TeamMemberCard image={Image1} name={"Jan Kowalski"} desription={`          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.`} />
+        <TeamMemberCard image={Image1} name={"Jan Kowalski"} desription={`          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.`} />
+      </div>
+    </section>
+  );
+};
+
+export default Team;
