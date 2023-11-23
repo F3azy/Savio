539eb6d50ef5cf1826ca5b3eceab2fe14474697d
Francesco <francescocarvelli25@gmail.com>
2023-11-23 23:21:39 +0100

    added a const tab for contact section with contact options

--- src/constans/index.js
+++ src/constans/index.js
@@ -1,3 +1,4 @@
+//gallery const
 const YTVideos = [
   "https://www.youtube.com/embed/u1kDmE2Ou3I",
   "https://www.youtube.com/embed/PfqsHIITt8c",
@@ -36,9 +37,8 @@ const YTThumbnail = [
 
 export { YTThumbnail, YTVideos };
 
-
-import { FaFacebook, FaYoutube  } from "react-icons/fa";
-
+// footer const
+import { FaFacebook, FaYoutube } from "react-icons/fa";
 export const footerSocialIcons = [
   {
     name: "facebook",
@@ -50,4 +50,28 @@ export const footerSocialIcons = [
     icon: FaYoutube,
     url: "https://www.youtube.com/@zespomuzycznyszczecinsavio6379",
   },
+];
+
+// contact const
+import { FaPhone } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { FaLocationDot } from "react-icons/fa6";
+
+export const contacts = [
+  {
+    name: "phone",
+    icon: FaPhone,
+    value: "+48 123 456 789",
+  },
+  {
+    name: "mail",
+    icon: IoIosMail,
+    value: "przyklad@mail.com",
+  },
+  {
+    name: "location",
+    icon: FaLocationDot,
+    value:
+    "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d180800.9017067485!2d14.554698902963679!3d53.458733290241554!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47aa093800d3a759%3A0xa95adc4e5f8ac4f3!2sSzczecin%2C%20Polska!5e0!3m2!1spl!2sus!4v1700777620050!5m2!1spl!2sus"
+    },
 ];
