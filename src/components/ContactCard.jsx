4a556a747e32d51f17f0fa9febdf6d425a9574fd
Francesco <francescocarvelli25@gmail.com>
2023-11-23 23:24:06 +0100

    added the contact card component for contact section

---
+++ src/components/ContactCard.jsx
@@ -0,0 +1,42 @@
+const ContactCard = ({ Icon, value, name }) => {
+  return (
+    <div
+      className={`
+    ${name==="location" ? "col-span-2 p-1" : "p-8"}
+    relative 
+    border-2
+    rounded-lg
+     border-brand-secondary/30 
+     bg-[#FCFCFC]`}
+    >
+      <div
+        className="
+      p-2
+      rounded-full
+      border-2
+      border-brand-secondary/30
+      absolute 
+      -top-[32px] 
+      left-[50%]
+      translate-x-[-50%]
+      bg-[#FCFCFC]
+      "
+      >
+        <Icon className="w-[40px] h-[40px]" />
+      </div>
+      {name === "location" ? (
+        <iframe
+          src={value}
+          className="w-full h-[500px] rounded-md"
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      ) : (
+        <p className="text-xl tracking-wider text-center">{value}</p>
+      )}
+    </div>
+  );
+};
+
+export default ContactCard;
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ export { default as Modal } from "./Modal";
 export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
 export { default as TeamMemberCard } from "./TeamMemberCard";
+export { default as ContactCard } from "./ContactCard";
 export { default as Carousel } from "./Carousel";
 export { default as ScrollToTop } from "./SrollToTop";
